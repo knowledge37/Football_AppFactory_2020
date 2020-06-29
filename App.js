87fe9b65c5a272853217fdf5728719d3d1b2d2c6
knowledge Ngobeni <knowledge@tshimologong.joburg>
2020-06-29 22:29:45 +0200

    bb

--- App.js
+++ App.js
@@ -1,1 +1,13 @@
-alert("");
+const url='https://api.wheretheiss.at/v1/satellites';
+
+async function getData(){
+    const resp= await fetch(url);
+
+    const data=JSON.parse(resp);
+    console.log(data)
+const {latitude,longitude}=data;
+document.getElementById('lat').textContent=latitude;
+document.getElementById('lon').textContent=longitude;
+}
+
+getData;
