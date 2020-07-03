d1014b26cb0cfd1c973d335d2175938fd2d71333
knowledge Ngobeni <knowledge@tshimologong.joburg>
2020-07-03 13:47:05 +0200

    adding api and litle of css

--- App.js
+++ App.js
@@ -1,81 +1,40 @@
-const url='https://api.wheretheiss.at/v1/satellites';
+const app = document.getElementById('root')
 
-async function getData(){
-    const resp= await fetch(url);
+const logo = document.createElement('img')
+logo.src = 'img/images (4).jpeg'
 
-    const data=JSON.parse(resp);
-    console.log(data)
-const {latitude,longitude}=data;
-document.getElementById('lat').textContent=latitude;
-document.getElementById('lon').textContent=longitude;
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+//app.appendChild(logo)
+app.appendChild(container)
+
+var request = new XMLHttpRequest()
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach((movie) => {
+      const card = document.createElement('div')
+      card.setAttribute('class', 'card')
+
+      const h1 = document.createElement('h1')
+      h1.textContent = movie.title
+
+      const p = document.createElement('p')
+      movie.description = movie.description.substring(0, 300)
+      p.textContent = `${movie.description}...`
+
+      container.appendChild(card)
+      card.appendChild(h1)
+      card.appendChild(p)
+    })
+  } else {
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Gah, it's not working!`
+    app.appendChild(errorMessage)
+  }
 }
 
-getData();
-// selector
-let player1 = document.querySelector('#player1');
-
-//storage
-let arrayPlayer=[];
-
-//default loader
-window.addEventListener('DOMContentLoaded',(e)=>{
-    getInfo();
-});
-
-// method for a request
-function getInfo(){
-    while (getInfo.hasChildNodes()) {
-        getInfo.removeChild(getInfo.lastChild)
-    }
-  const request = new XMLHttpRequest();
-   request.open("GET",'https://api.wheretheiss.at/v1/satellites');
-
-   request.onload = function(){
-    let ladumaNews = JSON.parse(request.responseText);
-    arrayPlayer = ladumaNews.arrayPlayer;
-   }
-    arrayPlayer = ladumaNews.arrayPlayer;
-    articles.forEach(article => {
-
-        let card = document.createElement('div');
-        card.classList.add('card'); 
-        card.style.width = '70%'; 
-        card.style.margin = '5% 0 0 0'
-        card.style.border = '1px solid #CFD8DC'
-        card.style.borderRadius = '5px';
-        let cardImage = document.createElement('img'); 
-        cardImage.classList.add('card-img-top'); 
-        cardImage.src = article.urlToImage;
-        cardImage.style.width = '100%';
-        card.appendChild(cardImage);
-        let cardBodyTitle = document.createElement('h6');
-        cardBodyTitle.classList.add('card-title'); 
-        cardBodyTitle.textContent = article.title;
-        cardBodyTitle.style.padding
-        let cardBodyText = document.createElement('h6');
-        cardBodyText.classList.add('card-text'); 
-        cardBodyText.textContent = article.content;
-        let cardBody = document.createElement('div');
-        cardBody.classList.add = "card-body";
-        cardBody.style.margin = '0.5em';
-        // cardBody.style.textAlign = 'center';
-        let button = document.createElement('a');
-        button.style.textAlign = 'center';
-        button.href = article.url;
-        button.style.position = 'absolute'; 
-        button.style.bottom = 0;
-        button.style.textDecoration = 'underline';
-        button.innerHTML = '<i class="fas fa-link"></i>'+'Read more';
-        cardBody.appendChild(cardBodyTitle);
-        cardBody.appendChild(cardBodyText);  
-        cardBody.appendChild(button);
-        card.appendChild(cardBody);
-        
-       healths.append(card);
-    });
-
-
-
-request.send(null);
-       
-}
+request.send()
